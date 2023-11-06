84034455e09b3835ff3732c700ad52dceafce09e
VolodymyrKucheruk <voloda.2014a@gmail.com>
2023-11-06 20:07:18 +0100

    modefied1

--- js/task-01.js
+++ js/task-01.js
@@ -4,9 +4,9 @@ const categories = categoriesList.querySelectorAll(".item");
 console.dir(`Number of categories: ${categories.length}`);
 // Застосовуємо  метод forEach() і виводимо в консоль імена категорій і кількість елементів.
 categories.forEach((category) => {
-  const categoryTitle = category.querySelector("h2").textContent;
-  const categoryItems = category.querySelectorAll("li").length;
-  
+  const categoryTitle = category.firstElementChild.textContent;
+  const categoryItems = category.lastElementChild.children.length;
+
   console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${categoryItems}`);
 });
